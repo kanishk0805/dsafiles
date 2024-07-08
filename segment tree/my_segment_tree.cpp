0d9329d3293f430549232096ed1c5f194c6889a5
kanishk0805 <kanishkgupta071@gmail.com>
2024-07-08 22:08:29 +0530

    dubugged seg tree

--- segment tree/my_segment_tree.cpp
+++ segment tree/my_segment_tree.cpp
@@ -3,15 +3,10 @@ using namespace std;
 class SegmentTree{
     vector<int>  ST;
     int n;
-    public:
-    SegmentTree(vector<int>& arr){
-        n = arr.size();
-        ST.resize(4*n);
-        buildSegmentTree(arr,1,0,n-1);
-    }
-    
     void buildSegmentTree(vector<int> &A,int si,int ss,int se) {
+        // cout<<"creating"<<endl;
         if (ss == se) {
+            // cout<<A[ss]<<' ';
             ST[si] = A[ss];
             return ;
         }
@@ -23,58 +18,78 @@ class SegmentTree{
     // Return sum of values in
     }
     void update(int si,int ss,int se,int index,int val){
+        if(index<ss || index>se) return;
         if(index==ss && index==se){
             ST[si] = val;
             return ;
         }
         int mid=(ss+se)/2;
-        if(mid<index) update(si*2,mid+1,se,index,val);
-        else  update(si*2+1,ss,mid,index,val);
+        update(si*2,ss,mid,index,val);
+        update(si*2+1,mid+1,se,index,val);
         ST[si]=ST[si*2]+ST[si*2+1];
         return ;
     }
-    void updateval(int indx,int val){
-        update(1,0,n-1,indx,val);
-    }
     int givesum(int si,int ss,int se,int l,int r){
-        if(ss<l || se>r) return 0;
+        if(se<l || ss>r) return 0;
         
         if(ss>=l && se<=r) return ST[si];
         int mid = (ss + se)/2;
         return givesum(si*2,ss,mid,l,r)+givesum(si*2+1,mid+1,se,l,r);
     }
+    public:
+    SegmentTree(vector<int>& arr){
+        n = arr.size();
+        ST.resize(4*n);
+        buildSegmentTree(arr,1,0,n-1);
+    }
+    
+    void updateval(int indx,int val){
+        update(1,0,n-1,indx,val);
+    }
     int query(int l,int r){
+        // cout<<l<<' '<<r<<endl;
         return givesum(1,0,n-1,l,r);
     }
     void printSegmentTree(){
-        int t=0;
-        auto it=++ST.begin();
-        auto end=next(ST.begin(),2*n);
-        int i=0;
-        while(it!=end){
-            cout<<*it<<' ';
-            if(i==t){
-                cout<<endl;
-                t<<1;
+        int t=1;
+        queue<int> q;
+        q.push(1);
+        while(!q.empty()){
+            int size=q.size();
+            while(size--){
+                t=q.front();
+                cout<<ST[t]<<' ';
+                q.pop();
+                if(t*2<n*2) q.push(t*2);
+                if(t*2+1<n*2) q.push(t*2+1);
+                t++;
             }
-            ++i;
-            ++it;
+            cout<<endl;
         }
-        cout<<endl;
+
+        // cout<<endl;
     }
 };
 
 
 int main(){
-    int n;
+    int n=5;
     // cin>>n;
     vector<int> v={1,2,3,4,5};
     // for(auto &i : v) cin>>i;
     SegmentTree st(v);
+    for(int i=0;i<n;i++){
+        cout<<st.query(i,i)<<' ';
+    }
+    cout<<endl;
     st.printSegmentTree();
-    v[3]=5;
-    // st.update(0,0,n-1,3);
+    st.updateval(3,8);
+    for(int i=0;i<n;i++){
+        cout<<st.query(i,i)<<' ';
+    }
+    cout<<endl;
     st.printSegmentTree();
-    cout<<st.givesum(0,0,n-1,0,3)<<endl;
+    
+    
 
 }
